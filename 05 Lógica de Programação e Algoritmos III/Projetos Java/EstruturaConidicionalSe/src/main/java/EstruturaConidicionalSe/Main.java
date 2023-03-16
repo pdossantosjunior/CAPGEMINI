3786a9b68a34fcba5f602a4fadca67f1a1a1eac4
Paulo César <paulo_heleno@hotmail.com>
2023-03-16 17:06:15 -0300

    20230316-1

---
+++ 05 Lógica de Programação e Algoritmos III/Projetos Java/EstruturaConidicionalSe/src/main/java/EstruturaConidicionalSe/Main.java
@@ -0,0 +1,43 @@
+package EstruturaConidicionalSe;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+       int media = 7;
+        
+       if(media >= 7){
+           //Aprovado
+       }else{
+           //Reprovado           
+       } 
+       
+       if(media >= 7){
+           if(media == 10){
+               //Parabéns, aprovado com nota máxima!
+           }else{
+               //Aprovado!
+           }
+       }else{
+           //Reprovado           
+       }     
+       
+       /**
+        * Operadores
+        * 
+        * > Maior
+        * >= Maior e igual
+        * < Menor
+        * <= Menor e igual
+        * == igual
+        * != diferente
+        * 
+        * && and
+        * || or
+        * ! negative
+        */
+       
+        
+    }
+    
+}
---
+++ 05 Lógica de Programação e Algoritmos III/Projetos Java/VariaveisTiposOperadores/src/main/java/VariaveisTiposOperadores/Main.java
@@ -0,0 +1,28 @@
+package VariaveisTiposOperadores;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        //Variáveis primitivas
+        int idade = 0;      //números inteiros
+        float cotacaoDolar = 5.0f;     //números reais - ponto flutuante    //coloca o "f" ao incializar a variável do tipo float
+        double cotacaoEuro = 6.0d;     //decimais       //coloca o "d" ao incializar a variável do tipo double
+        char genero = 'M';        //apenas uma letra //Em double usa-se as aspas simples ''
+        byte placar = 0;       //números inteiros
+        boolean estaCadastrado = false;        //valores lógicos - true/false
+        
+        //Variáveis não primitivas
+        String nome = "Texto armazenado em uma variável String";    //Em String usa-se as aspas duplas ""
+        
+        
+        +   //Adição
+        -   //Subtração
+        /   //Divisão
+        *   //Multuplicação
+        %   //Resto     
+        
+    }
+    
+}
